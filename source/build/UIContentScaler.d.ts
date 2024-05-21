68a7aba0cd0d24748ae2a4c59863faeddb394c3d
guzhu@fairygui.com <guzhu@fairygui.com>
2024-05-21 23:39:43 +0800

    delete temp result

--- source/build/UIContentScaler.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../lib/cc.d.ts" />
-import { Size } from "cc";
-export declare class UIContentScaler {
-    static scaleFactor: number;
-    static scaleLevel: number;
-    static rootSize: Size;
-}
-export declare function updateScaler(): void;
--- source/build/utils/ToolSet.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../lib/cc.d.ts" />
-import { Color } from "cc";
-export declare function toGrayedColor(c: Color): Color;
